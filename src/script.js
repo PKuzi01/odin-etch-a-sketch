0bf57b01e16e4d8c1e88f446f136fb81d22bac76
Philisizwe Kuzwayo <cre8storeese@gmail.com>
2023-06-06 20:41:12 +0200

    Built the grid

---
+++ src/script.js
@@ -0,0 +1,16 @@
+let container = document.createElement(`div`);
+    container.classList.add(`container`);
+    document.body.appendChild(container);
+
+for (let x = 0; x < 16; x++) {
+    let row = document.createElement(`div`);
+    row.classList.add(`row`);
+
+    for (let y= 0; y < 16; y++) {
+        let item = document.createElement(`div`);
+        item.classList.add(`cell`);
+        row.appendChild(item);
+    }
+
+    container.appendChild(row);
+};
